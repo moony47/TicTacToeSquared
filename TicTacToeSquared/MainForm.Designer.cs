76275e73f7909abd2bac85cbc2b98e9d7395c625
Sam <sfmoon47@gmail.com>
2024-02-29 16:44:22 +0000

    First Commit

---
+++ TicTacToeSquared/MainForm.Designer.cs
@@ -0,0 +1,282 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: sfmoo
+ * Date: 09/02/2020
+ * Time: 01:32
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace TicTacToeSquared
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.lbl0 = new System.Windows.Forms.Label();
+			this.lbl1 = new System.Windows.Forms.Label();
+			this.lbl2 = new System.Windows.Forms.Label();
+			this.lbl5 = new System.Windows.Forms.Label();
+			this.lbl4 = new System.Windows.Forms.Label();
+			this.lbl3 = new System.Windows.Forms.Label();
+			this.lbl6 = new System.Windows.Forms.Label();
+			this.lbl8 = new System.Windows.Forms.Label();
+			this.lbl7 = new System.Windows.Forms.Label();
+			this.lblTurn = new System.Windows.Forms.Label();
+			this.btnStart = new System.Windows.Forms.Button();
+			this.txtPeerID = new System.Windows.Forms.TextBox();
+			this.btnConnect = new System.Windows.Forms.Button();
+			this.btnCreate = new System.Windows.Forms.Button();
+			this.lblIP = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// lbl0
+			// 
+			this.lbl0.BackColor = System.Drawing.Color.Black;
+			this.lbl0.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.lbl0.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lbl0.ForeColor = System.Drawing.Color.White;
+			this.lbl0.Location = new System.Drawing.Point(12, 12);
+			this.lbl0.Name = "lbl0";
+			this.lbl0.RightToLeft = System.Windows.Forms.RightToLeft.No;
+			this.lbl0.Size = new System.Drawing.Size(108, 108);
+			this.lbl0.TabIndex = 0;
+			this.lbl0.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.lbl0.Visible = false;
+			// 
+			// lbl1
+			// 
+			this.lbl1.BackColor = System.Drawing.Color.Black;
+			this.lbl1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lbl1.ForeColor = System.Drawing.Color.White;
+			this.lbl1.Location = new System.Drawing.Point(134, 12);
+			this.lbl1.Name = "lbl1";
+			this.lbl1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+			this.lbl1.Size = new System.Drawing.Size(108, 108);
+			this.lbl1.TabIndex = 1;
+			this.lbl1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.lbl1.Visible = false;
+			// 
+			// lbl2
+			// 
+			this.lbl2.BackColor = System.Drawing.Color.Black;
+			this.lbl2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lbl2.ForeColor = System.Drawing.Color.White;
+			this.lbl2.Location = new System.Drawing.Point(256, 12);
+			this.lbl2.Name = "lbl2";
+			this.lbl2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+			this.lbl2.Size = new System.Drawing.Size(108, 108);
+			this.lbl2.TabIndex = 2;
+			this.lbl2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.lbl2.Visible = false;
+			// 
+			// lbl5
+			// 
+			this.lbl5.BackColor = System.Drawing.Color.Black;
+			this.lbl5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.lbl5.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lbl5.ForeColor = System.Drawing.Color.White;
+			this.lbl5.Location = new System.Drawing.Point(256, 134);
+			this.lbl5.Name = "lbl5";
+			this.lbl5.RightToLeft = System.Windows.Forms.RightToLeft.No;
+			this.lbl5.Size = new System.Drawing.Size(108, 108);
+			this.lbl5.TabIndex = 5;
+			this.lbl5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.lbl5.Visible = false;
+			// 
+			// lbl4
+			// 
+			this.lbl4.BackColor = System.Drawing.Color.Black;
+			this.lbl4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.lbl4.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lbl4.ForeColor = System.Drawing.Color.White;
+			this.lbl4.Location = new System.Drawing.Point(134, 134);
+			this.lbl4.Name = "lbl4";
+			this.lbl4.RightToLeft = System.Windows.Forms.RightToLeft.No;
+			this.lbl4.Size = new System.Drawing.Size(108, 108);
+			this.lbl4.TabIndex = 4;
+			this.lbl4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.lbl4.Visible = false;
+			// 
+			// lbl3
+			// 
+			this.lbl3.BackColor = System.Drawing.Color.Black;
+			this.lbl3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.lbl3.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lbl3.ForeColor = System.Drawing.Color.White;
+			this.lbl3.Location = new System.Drawing.Point(12, 134);
+			this.lbl3.Name = "lbl3";
+			this.lbl3.RightToLeft = System.Windows.Forms.RightToLeft.No;
+			this.lbl3.Size = new System.Drawing.Size(108, 108);
+			this.lbl3.TabIndex = 3;
+			this.lbl3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.lbl3.Visible = false;
+			// 
+			// lbl6
+			// 
+			this.lbl6.BackColor = System.Drawing.Color.Black;
+			this.lbl6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.lbl6.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lbl6.ForeColor = System.Drawing.Color.White;
+			this.lbl6.Location = new System.Drawing.Point(12, 256);
+			this.lbl6.Name = "lbl6";
+			this.lbl6.RightToLeft = System.Windows.Forms.RightToLeft.No;
+			this.lbl6.Size = new System.Drawing.Size(108, 108);
+			this.lbl6.TabIndex = 6;
+			this.lbl6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.lbl6.Visible = false;
+			// 
+			// lbl8
+			// 
+			this.lbl8.BackColor = System.Drawing.Color.Black;
+			this.lbl8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.lbl8.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lbl8.ForeColor = System.Drawing.Color.White;
+			this.lbl8.Location = new System.Drawing.Point(256, 256);
+			this.lbl8.Name = "lbl8";
+			this.lbl8.RightToLeft = System.Windows.Forms.RightToLeft.No;
+			this.lbl8.Size = new System.Drawing.Size(108, 108);
+			this.lbl8.TabIndex = 8;
+			this.lbl8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.lbl8.Visible = false;
+			// 
+			// lbl7
+			// 
+			this.lbl7.BackColor = System.Drawing.Color.Black;
+			this.lbl7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.lbl7.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lbl7.ForeColor = System.Drawing.Color.White;
+			this.lbl7.Location = new System.Drawing.Point(134, 256);
+			this.lbl7.Name = "lbl7";
+			this.lbl7.RightToLeft = System.Windows.Forms.RightToLeft.No;
+			this.lbl7.Size = new System.Drawing.Size(108, 108);
+			this.lbl7.TabIndex = 7;
+			this.lbl7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.lbl7.Visible = false;
+			// 
+			// lblTurn
+			// 
+			this.lblTurn.BackColor = System.Drawing.Color.Black;
+			this.lblTurn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblTurn.ForeColor = System.Drawing.Color.White;
+			this.lblTurn.Location = new System.Drawing.Point(134, 377);
+			this.lblTurn.Name = "lblTurn";
+			this.lblTurn.Size = new System.Drawing.Size(108, 53);
+			this.lblTurn.TabIndex = 9;
+			this.lblTurn.Text = "X Turn";
+			this.lblTurn.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.lblTurn.Visible = false;
+			// 
+			// btnStart
+			// 
+			this.btnStart.Enabled = false;
+			this.btnStart.Location = new System.Drawing.Point(134, 134);
+			this.btnStart.Name = "btnStart";
+			this.btnStart.Size = new System.Drawing.Size(108, 54);
+			this.btnStart.TabIndex = 10;
+			this.btnStart.Text = "Start Game";
+			this.btnStart.UseVisualStyleBackColor = true;
+			this.btnStart.Click += new System.EventHandler(this.StartGameClick);
+			// 
+			// txtPeerID
+			// 
+			this.txtPeerID.Location = new System.Drawing.Point(69, 100);
+			this.txtPeerID.Name = "txtPeerID";
+			this.txtPeerID.Size = new System.Drawing.Size(243, 20);
+			this.txtPeerID.TabIndex = 11;
+			// 
+			// btnConnect
+			// 
+			this.btnConnect.Location = new System.Drawing.Point(204, 12);
+			this.btnConnect.Name = "btnConnect";
+			this.btnConnect.Size = new System.Drawing.Size(108, 54);
+			this.btnConnect.TabIndex = 13;
+			this.btnConnect.Text = "Connect to Server";
+			this.btnConnect.UseVisualStyleBackColor = true;
+			this.btnConnect.Click += new System.EventHandler(this.ConnectToServer);
+			// 
+			// btnCreate
+			// 
+			this.btnCreate.Location = new System.Drawing.Point(69, 12);
+			this.btnCreate.Name = "btnCreate";
+			this.btnCreate.Size = new System.Drawing.Size(108, 54);
+			this.btnCreate.TabIndex = 14;
+			this.btnCreate.Text = "Create Server";
+			this.btnCreate.UseVisualStyleBackColor = true;
+			this.btnCreate.Click += new System.EventHandler(this.CreateServer);
+			// 
+			// lblIP
+			// 
+			this.lblIP.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblIP.Location = new System.Drawing.Point(69, 79);
+			this.lblIP.Name = "lblIP";
+			this.lblIP.Size = new System.Drawing.Size(243, 18);
+			this.lblIP.TabIndex = 15;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(381, 439);
+			this.Controls.Add(this.lblIP);
+			this.Controls.Add(this.btnCreate);
+			this.Controls.Add(this.btnConnect);
+			this.Controls.Add(this.txtPeerID);
+			this.Controls.Add(this.btnStart);
+			this.Controls.Add(this.lblTurn);
+			this.Controls.Add(this.lbl8);
+			this.Controls.Add(this.lbl7);
+			this.Controls.Add(this.lbl6);
+			this.Controls.Add(this.lbl5);
+			this.Controls.Add(this.lbl4);
+			this.Controls.Add(this.lbl3);
+			this.Controls.Add(this.lbl2);
+			this.Controls.Add(this.lbl1);
+			this.Controls.Add(this.lbl0);
+			this.Name = "MainForm";
+			this.Text = "TicTacToeSquared";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Label lblIP;
+		private System.Windows.Forms.Button btnCreate;
+		private System.Windows.Forms.Button btnConnect;
+		private System.Windows.Forms.TextBox txtPeerID;
+		private System.Windows.Forms.Button btnStart;
+		private System.Windows.Forms.Label lblTurn;
+		private System.Windows.Forms.Label lbl7;
+		private System.Windows.Forms.Label lbl8;
+		private System.Windows.Forms.Label lbl6;
+		private System.Windows.Forms.Label lbl3;
+		private System.Windows.Forms.Label lbl4;
+		private System.Windows.Forms.Label lbl5;
+		private System.Windows.Forms.Label lbl2;
+		private System.Windows.Forms.Label lbl1;
+		private System.Windows.Forms.Label lbl0;
+	}
+}
---
+++ TicTacToeSquared/MainForm.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace TicTacToeSquared {
+	public partial class MainForm : Form {
+		
+		Button[,] grid = new Button[9,9];
+		string[] cellCross = new string[9];
+		bool crossTurn = true;
+		StreamReader sr;
+		StreamWriter sw;
+		bool isServer = false;
+		TcpClient client;
+		
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+		
+		void CreateServer (object sender, EventArgs e) {
+		    try {
+				string ip = new WebClient().DownloadString("http://api.ipify.org");
+				IPAddress externalIP = IPAddress.Parse(ip);
+
+                lblIP.Text = ip;
+                MessageBox.Show("Waiting for connection. Host name: " + ip + " This has been copied to your clipboard.");
+				Clipboard.SetText(ip);
+				
+				Update();
+				
+				TcpListener listener = new TcpListener(3000);
+				listener.Start();
+				client = listener.AcceptTcpClient();
+				sr = new StreamReader(client.GetStream());
+				sw = new StreamWriter(client.GetStream());
+				
+				MessageBox.Show("Client Found");
+				isServer = true;
+				
+				btnConnect.Visible = false;
+				btnCreate.Visible = false;
+				btnStart.Enabled = true;
+			} catch (Exception ex) {
+				MessageBox.Show("Setup failed\n" + ex);
+			}
+		}
+		
+		void ConnectToServer (object sender, EventArgs e) {
+			try {
+				
+				string peerID = txtPeerID.Text;
+				client = new TcpClient(peerID, 3000);
+				sr = new StreamReader(client.GetStream());
+				sw = new StreamWriter(client.GetStream());
+				
+				lblIP.Text = Dns.GetHostEntry(Dns.GetHostName()).AddressList[0].ToString();
+				
+				MessageBox.Show("Server Found");
+				isServer = false;
+				
+				btnConnect.Visible = false;
+				btnCreate.Visible = false;
+				
+				if (sr.ReadLine() == "STARTGAME") {
+				    sr.DiscardBufferedData();
+				    StartGame();
+				}
+				
+				WaitForTurn();
+			} catch {
+				MessageBox.Show("Server not found, try again.");
+			}
+		}
+		
+		void DisableAll () {
+			for (int x = 0; x < 9; x++) {
+				for (int y = 0; y < 9; y++) {
+					grid[x,y].Enabled = false;
+					grid[x,y].BackColor = Color.DarkGray;
+				}
+			}
+		}
+		
+		void WaitForTurn () {
+			DisableAll();
+			Update();
+			
+			try {
+			int step = 0;
+			while (crossTurn != isServer) {
+				string data = sr.ReadLine();
+				
+				if (data == null)
+				    continue;
+				
+				if (step == 0) {
+					string[] splitData = data.Split(';');
+					
+					for (int x = 0; x < 9; x++) {
+						for (int y = 0; y < 9; y++) {
+							grid[x,y].Text = splitData[x*9 + y];
+						}
+					}
+					
+					step++;
+				} else if (step == 1) {
+					string[] splitData = data.Split(';');
+					
+					for (int x = 0; x < 9; x++) {
+						cellCross[x] = splitData[x];
+					}
+					
+					step++;
+				} else if (step == 2) {
+					string[] splitData = data.Split(';');
+					
+					bool won = false;
+					if (CheckCell(int.Parse(splitData[0]), int.Parse(splitData[1]), splitData[2]) == true){
+						cellCross[int.Parse(splitData[0])] = splitData[2];
+						won = CheckBigCell(int.Parse(splitData[0]), splitData[2]);
+					}
+					LockToCell (int.Parse(splitData[1]));
+					DrawBigCells(int.Parse(splitData[0]));
+					crossTurn = !crossTurn;
+					lblTurn.Text = (crossTurn?"X":"O") + " Turn";
+					
+					Update();
+						
+					if (won == true){
+						MessageBox.Show (splitData[2] + " wins the game");
+						
+						sr.Close();
+						sw.Close();
+						Application.Restart();
+					}
+					
+					step++;
+				}
+			}
+			} catch {
+				
+			}
+		}
+		
+		void StartGameClick (object sender, EventArgs e) {
+			StartGame();
+			
+			sw.WriteLine("STARTGAME");
+			sw.Flush();
+		}
+		
+		void StartGame () {
+			for (int x = 0; x < 9; x++) {
+				((Label) Controls.Find("lbl" + x, false)[0]).Visible = false;
+			}
+			lblIP.Visible = false;
+			btnStart.Visible = false;
+			lblTurn.Visible = true;
+			txtPeerID.Visible = false;
+			CreateGrid();
+		}
+		
+		void TakeTurn (object sender, EventArgs e) {
+			if (((Button) sender).Text != "")
+				return;
+			
+			bool won = false;
+			int a = int.Parse(((Button) sender).Name[0].ToString());
+			int b = int.Parse(((Button) sender).Name[2].ToString());
+			string c = crossTurn?"X":"O";
+			((Button) sender).Text = c;
+			if (CheckCell(a, b, c) == true){
+				cellCross[a] = c;
+				won = CheckBigCell(a, c);
+			}
+			LockToCell (b);
+			DrawBigCells(a);
+			crossTurn = !crossTurn;
+			lblTurn.Text = (crossTurn?"X":"O") + " Turn";
+			
+			Update();
+			
+			TransmitCurrentGameState(a, b, c);
+			
+			if (won == true){
+				MessageBox.Show (c + " wins the game");
+				
+				sr.Close();
+				sw.Close();
+				Application.Restart();
+			}
+			
+			WaitForTurn();
+		}
+		
+		void TransmitCurrentGameState(int a, int b, string c) {
+			string data = "";
+			for (int x = 0; x < 9; x++) {
+				for (int y = 0; y < 9; y++) {
+					data += ";" + grid[x,y].Text;
+				}
+			}
+			
+			sw.WriteLine(data.Substring(1));
+			sw.Flush();
+			
+			data = "";
+			for (int x = 0; x < 9; x++) {
+				data += ";" + cellCross[x];
+			}
+			
+			sw.WriteLine(data.Substring(1));
+			sw.Flush();
+			
+			sw.WriteLine(a + ";" + b + ";" + c);
+			sw.Flush();
+		}
+		
+		bool CheckBigCell (int a, string c){
+			for (int x = 0; x < 3; x++) {
+				if (a%3 == x)
+					if (cellCross[x] == c && cellCross[3+x] == c && cellCross[6+x] == c) {
+					((Label) Controls.Find("lbl" + x, false)[0]).BackColor = Color.Green;
+					((Label) Controls.Find("lbl" + (3+x), false)[0]).BackColor = Color.Green;
+					((Label) Controls.Find("lbl" + (6+x), false)[0]).BackColor = Color.Green;
+					return true;
+				}
+				if (a/3 == x)
+					if (cellCross[3*x] == c && cellCross[1 + 3*x]== c && cellCross[2 + 3*x] == c) {
+					((Label) Controls.Find("lbl" + (3*x), false)[0]).BackColor = Color.Green;
+					((Label) Controls.Find("lbl" + (1 + 3*x), false)[0]).BackColor = Color.Green;
+					((Label) Controls.Find("lbl" + (2 + 3*x), false)[0]).BackColor = Color.Green;
+					return true;
+				}
+			}
+			if (a%4 == 0)
+				if (cellCross[0] == c && cellCross[4] == c && cellCross[8] == c){
+				((Label) Controls.Find("lbl0", false)[0]).BackColor = Color.Green;
+				((Label) Controls.Find("lbl4", false)[0]).BackColor = Color.Green;
+				((Label) Controls.Find("lbl8", false)[0]).BackColor = Color.Green;
+				return true;
+			}
+			if (a%2 == 0 && a != 0 && a != 8)
+				if (cellCross[2] == c && cellCross[4] == c && cellCross[6] == c){
+				((Label) Controls.Find("lbl2", false)[0]).BackColor = Color.Green;
+				((Label) Controls.Find("lbl4", false)[0]).BackColor = Color.Green;
+				((Label) Controls.Find("lbl6", false)[0]).BackColor = Color.Green;
+				return true;
+			}
+			
+			bool draw = true;
+			for (int x = 0; x < 9; x++)
+				if (cellCross[x] == "")
+					draw = false;
+			
+			if (draw == true) {
+				MessageBox.Show("The game is a draw.");
+				Application.Restart();
+			}
+			
+			return false;
+		}
+		
+		void DrawBigCells (int a) {
+			((Label) Controls.Find("lbl" + a, false)[0]).Visible = cellCross[a]!="";
+			((Label) Controls.Find("lbl" + a, false)[0]).Text = cellCross[a];
+		}
+		
+		bool CheckCell (int a, int b, string c) {
+			for (int x = 0; x < 3; x++) {
+				if (b%3 == x)
+					if (grid[a,x].Text == c && grid[a,3+x].Text == c && grid[a,6+x].Text == c)
+						return true;
+				if (b/3 == x)
+					if (grid[a,3*x].Text == c && grid[a,1 + 3*x].Text == c && grid[a,2 + 3*x].Text == c)
+						return true;
+			}
+			if (b%4 == 0)
+				if (grid[a,0].Text == c && grid[a,4].Text == c && grid[a,8].Text == c)
+					return true;
+			if (b%2 == 0 && b != 0 && b != 8)
+				if (grid[a,2].Text == c && grid[a,4].Text == c && grid[a,6].Text == c)
+					return true;
+			
+			bool draw = true;
+			for (int x = 0; x < 9; x++)
+				if (grid[a, x].Text == "")
+					draw = false;
+			
+			if (draw == true)
+				for (int x = 0; x < 9; x++)
+					grid[a,x].Text = "";
+			
+			return false;
+		}
+		
+		void LockToCell (int a) {
+			for (int x = 0; x < 9; x++) {
+				for (int y = 0; y < 9; y++) {
+					grid[x,y].Enabled = x==a || cellCross[a]!="";
+					grid[x,y].BackColor = (x==a || cellCross[a]!="")?Color.Black:Color.DarkGray;
+				}
+			}
+		}
+		
+		void CreateGrid (){
+			for (int x = 0; x < 9; x++) {
+				for (int y = 0; y < 9; y++) {
+					grid[x,y] = new Button();
+					grid[x,y].Name = x + "," + y;
+					grid[x,y].Text = "";
+					grid[x,y].FlatStyle = FlatStyle.Popup;
+					grid[x,y].BackColor = Color.Black;
+					grid[x,y].ForeColor = Color.White;
+					grid[x,y].Size = new Size (32, 32);
+					grid[x,y].Location = new Point (12 + 122*(x%3) + 38*(y%3), 12 + 122*(x/3) + 38*(y/3));
+					grid[x,y].Click += new System.EventHandler(TakeTurn);
+					grid[x,y].TextAlign = ContentAlignment.MiddleCenter;
+					grid[x,y].Font = new Font(FontFamily.GenericSansSerif, 14f, FontStyle.Bold);
+					Controls.Add(grid[x,y]);
+				}
+				cellCross[x] = "";
+			}
+		}
+	}
+}
